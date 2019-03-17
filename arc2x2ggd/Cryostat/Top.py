e62e2b4674baae028b610390780e052e71a3bafd
Hunter Sullivan <hcsullivan12@yahoo.com>
2019-03-16 20:22:39 -0500

    Added connectors.

--- arc2x2ggd/Cryostat/Top.py
+++ arc2x2ggd/Cryostat/Top.py
@@ -19,27 +19,45 @@ class TopBuilder(gegede.builder.Builder):
 
         # place the connectors
         sbs = self.get_builders()
-        midcon_lv  = sbs[0].get_volume()
-        #edgecon_lv = sbs[1].get_volume()
+        longcon_lv  = sbs[0].get_volume()
+        innercon_lv = sbs[1].get_volume()
 
-        # place middle connectors
-        midZ = -1*main_shape.dz + self.shifts[0][1]
-        midR = self.shifts[0][0]
+        # place long connectors
+        midZ = -1*main_shape.dz + self.shifts[2]
+        longR = self.shifts[0]
+        rot1 = geom.structure.Rotation(self.name+longcon_lv.name+ '_rot', '0.0deg', '0.0deg', '0.0deg')  
 
-        sb_pos1 = geom.structure.Position(self.name+midcon_lv.name+ '_pos1', midR, Q('0m'), midZ)     
-        sb_rot1 = geom.structure.Rotation(self.name+midcon_lv.name+ '_rot1', '0.0deg', '0.0deg', '90.0deg')                            
-        sb_pla1 = geom.structure.Placement(self.name+midcon_lv.name+'_pla1', volume=midcon_lv, pos=sb_pos1, rot=sb_rot1)
+        sb_pos1 = geom.structure.Position(self.name+longcon_lv.name+ '_pos1', -1*longR, Q('0m'), midZ)                               
+        sb_pla1 = geom.structure.Placement(self.name+longcon_lv.name+'_pla1', volume=longcon_lv, pos=sb_pos1, rot=rot1)
         main_lv.placements.append(sb_pla1.name)
 
-        sb_pos2 = geom.structure.Position(self.name+midcon_lv.name+ '_pos2', Q('0m'), midR, midZ)     
-        sb_rot2 = geom.structure.Rotation(self.name+midcon_lv.name+ '_rot2', '0.0deg', '0.0deg', '0.0deg')                            
-        sb_pla2 = geom.structure.Placement(self.name+midcon_lv.name+'_pla2', volume=midcon_lv, pos=sb_pos2, rot=sb_rot2)
+        sb_pos2 = geom.structure.Position(self.name+longcon_lv.name+ '_pos2', Q('0m'), Q('0m'), midZ)                                 
+        sb_pla2 = geom.structure.Placement(self.name+longcon_lv.name+'_pla2', volume=longcon_lv, pos=sb_pos2, rot=rot1)
         main_lv.placements.append(sb_pla2.name)
 
-        sb_pos3 = geom.structure.Position(self.name+midcon_lv.name+ '_pos3', -1*midR, Q('0m'), midZ)                           
-        sb_pla3 = geom.structure.Placement(self.name+midcon_lv.name+'_pla3', volume=midcon_lv, pos=sb_pos3, rot=sb_rot1)
+        sb_pos3 = geom.structure.Position(self.name+longcon_lv.name+ '_pos3', longR, Q('0m'), midZ)                           
+        sb_pla3 = geom.structure.Placement(self.name+longcon_lv.name+'_pla3', volume=longcon_lv, pos=sb_pos3, rot=rot1)
         main_lv.placements.append(sb_pla3.name)
 
-        sb_pos4 = geom.structure.Position(self.name+midcon_lv.name+ '_pos4', Q('0m'), -1*midR, midZ)                             
-        sb_pla4 = geom.structure.Placement(self.name+midcon_lv.name+'_pla4', volume=midcon_lv, pos=sb_pos4, rot=sb_rot2)
-        main_lv.placements.append(sb_pla4.name)
+        # place inner connectors
+        innerR = self.shifts[1]
+        steps = [-1*longR, Q('0m'), longR]
+        rot2 = geom.structure.Rotation(self.name+innercon_lv.name+ '_rot1', '0.0deg', '0.0deg', '0.0deg')  
+        rot3 = geom.structure.Rotation(self.name+innercon_lv.name+ '_rot2', '0.0deg', '0.0deg', '90.0deg')  
+
+        i = 0
+        for step in steps:
+            i = i + 1
+            sb_pos = geom.structure.Position(self.name+innercon_lv.name+ '_pos'+str(i), -1*innerR, step, midZ)
+            sb_pla = geom.structure.Placement(self.name+innercon_lv.name+'_pla'+str(i), volume=innercon_lv, pos=sb_pos, rot=rot1)
+            main_lv.placements.append(sb_pla.name)
+        for step in steps:
+            i = i + 1
+            sb_pos = geom.structure.Position(self.name+innercon_lv.name+ '_pos'+str(i), innerR, step, midZ)
+            sb_pla = geom.structure.Placement(self.name+innercon_lv.name+'_pla'+str(i), volume=innercon_lv, pos=sb_pos, rot=rot1)
+            main_lv.placements.append(sb_pla.name)            
+
+
+
+
+       
