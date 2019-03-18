4e21e8a64c34d3ea9f309498cb54246464b671cd
Hunter Sullivan <hcsullivan12@yahoo.com>
2019-03-18 07:29:33 -0500

    Added inlets.

--- arc2x2ggd/Cryostat/Container.py
+++ arc2x2ggd/Cryostat/Container.py
@@ -8,11 +8,13 @@ class ContainerBuilder(gegede.builder.Builder):
 
     #^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
     def configure( self, dz=None, rmax=None, rmin=None,
-                    positions=None, material=None, shifts=None, **kwds ):
+                    positions=None, material=None, connectionshifts=None,
+                    inletshifts=None, **kwds ):
         self.rmin, self.rmax, self.dz = (rmin, rmax, dz)
         self.material = material
         self.positions = positions
-        self.shifts = shifts
+        self.connectionshifts = connectionshifts
+        self.inletshifts = inletshifts
 
     #^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^
     def construct( self, geom ):
@@ -21,9 +23,11 @@ class ContainerBuilder(gegede.builder.Builder):
         cont_lv = geom.structure.Volume('vol'+cont_shape.name, material=self.material, shape=cont_shape)
         self.add_volume( cont_lv )
 
-        # we will handle the connections seperately
+        # we will handle the connections and inlets seperately
         sbs = self.get_builders()
-        conn_lv = sbs[-1].get_volume()
+        sbs_aux = sbs[-2:]
+        inlet_lv = sbs_aux[0].get_volume()
+        conn_lv  = sbs_aux[1].get_volume()
         sbs = sbs[:len(self.positions)]
 
         # place the first components
@@ -38,10 +42,10 @@ class ContainerBuilder(gegede.builder.Builder):
             cont_lv.placements.append(sb_pla.name)
 
         # place the connections
-        connR = self.shifts[2]
-        connAng = self.shifts[1]
-        connZ1 = self.shifts[0]
-        connZ2 = self.shifts[3]
+        connR = self.connectionshifts[2]
+        connAng = self.connectionshifts[1]
+        connZ1 = self.connectionshifts[0]
+        connZ2 = self.connectionshifts[3]
         # hard coding here!
         connR2 = connR - Q('9cm')
 
@@ -63,4 +67,23 @@ class ContainerBuilder(gegede.builder.Builder):
         conn3_rot = geom.structure.Rotation(self.name+conn_lv.name+'3_rot', '0.0deg', '-120.0deg', '0.0deg')                            
         conn3_pla = geom.structure.Placement(self.name+conn_lv.name+'3_pla', volume=conn_lv, pos=conn3_pos, rot=conn3_rot)
         cont_lv.placements.append(conn3_pla.name)
+
+        # place the inlets
+        inletR   = self.inletshifts[2]
+        inletAngs = self.inletshifts[1]
+        inletZ   = self.inletshifts[0]
+
+        pos = []
+        rot = []
+        for ang in inletAngs:
+            pos.append([inletR*np.cos(ang), inletR*np.sin(ang), inletZ])  
+            rot.append(Q('90deg') + ang)
+
+        for i in range(0,4):            
+            inlet_pos = geom.structure.Position(self.name+inlet_lv.name+str(i+1)+'_pos', pos[i][0], -1*pos[i][1], pos[i][2])     
+            inlet_rot = geom.structure.Rotation(self.name+inlet_lv.name+str(i+1)+'_rot', '90.0deg', -1*rot[i], '0.0deg')                            
+            inlet_pla = geom.structure.Placement(self.name+inlet_lv.name+str(i+1)+'_pla', volume=inlet_lv, pos=inlet_pos, rot=inlet_rot)
+            cont_lv.placements.append(inlet_pla.name)
+
+
 
